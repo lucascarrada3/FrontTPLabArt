fee17c500c5ff842e10c27cd34c708594e4ee55d
luki carrada <luki.carrada@gmail.com>
2024-05-21 01:43:22 -0300

    css e insumos avanzados

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ const API_URL = 'http://localhost:8080/api';
 type Insumo = {
   id: number;
   denominacion: string;
-  categoria: string;
   precioCompra: number;
   precioVenta: number;
-  stock: number;
+  cantidad: number;
+  stockMaximo: number;
+  esParaElaborar: boolean;
 };
+
 
 type ArticuloManufacturado = {
   id: number;
@@ -21,6 +23,7 @@ type ArticuloManufacturado = {
   precioVenta: number;
   insumos: { insumo: Insumo, cantidad: number }[];
   eliminado: boolean;
+  disponible: boolean;
 };
 
 function ArticuloManufacturado() {
@@ -37,7 +40,134 @@ function ArticuloManufacturado() {
   const [nuevoArticuloprecioVenta, setNuevoArticuloprecioVenta] = useState<number>(0);
   // const [insumosEliminados, setInsumosEliminados] = useState<Insumo[]>([]);
   // const [precioVentaTotalInsumos, setprecioVentaTotalInsumos] = useState<number>(0);
+  const [nuevoInsumo, setNuevoInsumo] = useState({ denominacion: '', precioCompra: 0, cantidad: 0, stockMaximo: 0 });
+  const [insumos, setInsumos] = useState<Insumo[]>([]);
+  const [mostrarModalInsumos, setMostrarModalInsumos] = useState(false);
+  const [mostrarModalAgregarInsumo, setMostrarModalAgregarInsumo] = useState(false);
   const [criterio, setCriterio] = useState("id");
+  const [insumosDisponibles, setInsumosDisponibles] = useState([]);
+  const [mostrarFormularioNuevoInsumo, setMostrarFormularioNuevoInsumo] = useState(false); 
+  const [precioTotalArticulo, setPrecioTotalArticulo] = useState<number>(0); // Estado para el precio total del artículo manufacturado
+  
+
+
+  ///////////////////// INSUMOS/////////
+
+  useEffect(() => {
+    fetchInsumos();
+  }, []);
+
+  const fetchInsumos = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/articulosinsumos');
+      if (!response.ok) {
+        throw new Error('Error al obtener insumos');
+      }
+      const data = await response.json();
+      setInsumos(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleAbrirModalInsumos = async (id: number) => {
+    try {
+      const response = await fetch(`http://localhost:8080/api/articulomanufacturados/${id}`);
+      if (!response.ok) {
+        throw new Error('Error al obtener los datos del artículo manufacturado');
+      }
+      const data = await response.json();
+      const insumos = data.articuloManufacturadoDetalles.map((d: { articuloInsumo: any; }) => d.articuloInsumo);
+      console.log(insumos); // Verifica que obtienes los datos de los insumos correctamente
+      setInsumos(insumos);
+      setMostrarModalInsumos(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  
+  
+
+  const handleCerrarModalInsumos = () => {
+    setMostrarModalInsumos(false);
+  };
+
+  const handleAbrirModalAgregarInsumo = () => {
+    setMostrarModalAgregarInsumo(true);
+  };
+
+  const handleCerrarModalAgregarInsumo = () => {
+    setMostrarModalAgregarInsumo(false);
+  };
+
+  
+
+
+  //////////////////////Agregar INSUMOS///////////////////////////
+
+
+  const fetchInsumosDisponibles = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/articulosinsumos');
+      if (!response.ok) {
+        throw new Error('Error al obtener los insumos disponibles');
+      }
+      const data = await response.json();
+      setInsumosDisponibles(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  // Llama a la función fetchInsumosDisponibles en el useEffect para obtener los insumos disponibles al cargar el componente
+  useEffect(() => {
+    fetchInsumosDisponibles();
+  }, []);
+
+  // const handleInputChange = (e: { target: { name: any; value: any; }; }) => {
+  //   const { name, value } = e.target;
+  //   // Actualiza el estado del nuevo insumo
+  //   setNuevoInsumo((prevInsumo) => ({
+  //     ...prevInsumo,
+  //     [name]: value,
+  //   }));
+  //   // Realiza una llamada a la API para obtener la información completa del insumo seleccionado
+  //   if (value !== "Nuevo") {
+  //     fetchInsumoById(parseInt(value));
+  //   }
+  // };
+
+  const handleAgregarInsumo = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+  
+    if (nuevoInsumo.denominacion !== "") {
+      // Verifica si el nuevo insumo ya existe en la lista de insumos
+      const existeInsumo = insumosDisponibles.find(insumo => insumo.denominacion === nuevoInsumo.denominacion);
+      if (existeInsumo) {
+        // Si el insumo ya existe, simplemente agrégalo a la lista de insumos
+        setInsumos([...insumos, existeInsumo]);
+      } else {
+        // Si no existe, muestra un mensaje de error
+        console.error('El insumo seleccionado no se encontró en la lista de insumos disponibles.');
+      }
+    } else {
+      // Si no se ha seleccionado un insumo, muestra un mensaje de error
+      console.error('Por favor, selecciona un insumo válido.');
+    }
+  
+    // Restablece los valores del nuevo insumo y cierra el modal
+    setNuevoInsumo({
+      denominacion: '',
+      precioCompra: 0,
+      cantidad: 0,
+      stockMaximo: 0,
+    });
+    handleCerrarModalAgregarInsumo();
+  };
+  
+
+  ///////////////////////////ARTICULOS///////////////////////////
   
 //SOLICITUD GET
   useEffect(() => {
@@ -93,7 +223,8 @@ function ArticuloManufacturado() {
       descripcion: nuevoArticulodescripcion,
       tiempoEstimadoMinutos: nuevoArticulotiempoEstimadoMinutos,
       insumos: [],
-      eliminado: false
+      eliminado: false,
+      disponible: true
     };
 
     try {
@@ -283,7 +414,7 @@ function ArticuloManufacturado() {
                 <td>{articulo.tiempoEstimadoMinutos}</td>
                 <td>{articulo.eliminado ? "OK" : "NOT OK"}</td>
                 <td>
-                  {/* <button className="insumos" onClick={(null)}>Insumos</button> */}
+                  <button className="insumos" onClick={() => handleAbrirModalInsumos(articulo.id)}>Insumos</button>
                   <button className="modificar" onClick={() => handleModificarArticulo(articulo)}>✏️</button>
                   <button className="eliminar" onClick={() => handleEliminarArticulo(articulo.id)}>❌</button>
                   <button onClick={() => handleRestaurarDisponibilidad(articulo.id)}>✅</button>
@@ -370,24 +501,124 @@ function ArticuloManufacturado() {
         </div>
       )}
 
-      {/* {mostrarModalInsumos && articuloSeleccionado && (
-        <div className="modal">
-          <div className="modal-content">
-            <h3>Insumos del Artículo</h3>
-            <ul>
-              {articuloSeleccionado.insumos.map((insumo) => (
-                <li key={insumo.insumo.id}>
-                  {insumo.insumo.denominacion} - Cantidad: {insumo.cantidad}
-                  <button onClick={() => handleActualizarInsumo(insumo.insumo, -1)}>-</button>
-                  <button onClick={() => handleActualizarInsumo(insumo.insumo, 1)}>+</button>
-                </li>
-              ))}
-            </ul>
-            <button className="btn-guardar" onClick={handleCerrarModalInsumos}>Cerrar</button>
-            <button className="btn-cancelar" onClick={handleResetearContadorInsumo}>Resetear</button>
+{mostrarModalInsumos && (
+  <div className="modal">
+    <div className="modal-content">
+      <h3>Lista de Insumos</h3>
+      <button onClick={handleAbrirModalAgregarInsumo}>Agregar Insumo</button>
+      <table className='table-container2'>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Denominación</th>
+            <th>Precio Compra</th>
+            <th>Precio Venta</th>
+            <th>Cantidad</th>
+            <th>Stock Máximo</th>
+            <th>Es para Elaborar?</th>
+          </tr>
+        </thead>
+        <tbody>
+          {insumos.map((insumo) => (
+            <tr key={insumo.id}>
+              <td>{insumo.id}</td>
+              <td>{insumo.denominacion}</td>
+              <td>${insumo.precioCompra}</td>
+              <td>${insumo.precioVenta}</td>
+              <td>{insumo.cantidad}</td>
+              <td>{insumo.stockMaximo}</td>
+              <td>{insumo.esParaElaborar ? 'Sí' : 'No'}</td> {/* Mostrar "Sí" o "No" en lugar de true/false */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button className="btn-cerrar" onClick={handleCerrarModalInsumos}>Cerrar</button>
+    </div>
+  </div>
+)}
+
+{mostrarModalAgregarInsumo && (
+  <div className="modal">
+    <div className="modal-content">
+      <h3>Agregar Insumo</h3>
+      <form onSubmit={handleAgregarInsumo}>
+        <label>
+          Seleccionar insumo:
+          <select
+            value={mostrarFormularioNuevoInsumo ? "Nuevo" : nuevoInsumo.denominacion}
+            onChange={(e) => {
+              const selectedOption = e.target.value;
+              if (selectedOption === "Nuevo") {
+                // Mostrar el formulario para un nuevo insumo
+                setMostrarFormularioNuevoInsumo(true);
+                // Limpiar la denominación
+                setNuevoInsumo({ ...nuevoInsumo, denominacion: "" });
+              } else {
+                // Ocultar el formulario y establecer la opción seleccionada
+                setMostrarFormularioNuevoInsumo(false);
+                // Obtener la información del insumo seleccionado
+                const selectedInsumo = insumosDisponibles.find((insumo) => insumo.denominacion === selectedOption);
+                if (selectedInsumo) {
+                  setNuevoInsumo(selectedInsumo);
+                }
+              }
+            }}
+          >
+            <option value="">Seleccionar...</option>
+            {insumosDisponibles.map((insumo) => (
+              <option key={insumo.id} value={insumo.denominacion}>
+                {insumo.denominacion}
+              </option>
+            ))}
+            <option value="Nuevo">Nuevo</option> {/* Opción para agregar un nuevo insumo */}
+          </select>
+        </label>
+        {mostrarFormularioNuevoInsumo && (
+          <div>
+            <label>
+              Denominación:
+              <input
+                type="text"
+                value={nuevoInsumo.denominacion}
+                onChange={(e) => setNuevoInsumo({ ...nuevoInsumo, denominacion: e.target.value })}
+              />
+            </label>
+            <label>
+              Precio Compra:
+              <input
+                type="number"
+                value={nuevoInsumo.precioCompra}
+                onChange={(e) => setNuevoInsumo({ ...nuevoInsumo, precioCompra: parseFloat(e.target.value) })}
+              />
+            </label>
+            <label>
+              Cantidad:
+              <input
+                type="number"
+                value={nuevoInsumo.cantidad}
+                onChange={(e) => setNuevoInsumo({ ...nuevoInsumo, cantidad: parseInt(e.target.value) })}
+              />
+            </label>
+            <label>
+              Stock Máximo:
+              <input
+                type="number"
+                value={nuevoInsumo.stockMaximo}
+                onChange={(e) => setNuevoInsumo({ ...nuevoInsumo, stockMaximo: parseInt(e.target.value) })}
+              />
+            </label>
           </div>
-        </div>
-      )} */}
+        )}
+        <button type="submit">Guardar</button>
+        <button type="button" onClick={() => {
+          // Cerrar el modal y ocultar el formulario
+          setMostrarModalAgregarInsumo(false);
+          setMostrarFormularioNuevoInsumo(false);
+        }}>Cerrar</button>
+      </form>
+    </div>
+  </div>
+)}
     </div>
   );
 }
